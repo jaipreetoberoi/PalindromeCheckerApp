40cc8f0a54be1cbb69a3f5be683d6113a4e7c585
jaipreetoberoi <jaipreetsingh223@gmail.com>
2026-03-02 10:41:41 +0530

    UC4: Character Array Based Palindrome Check

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,30 +1,23 @@
-/*
-this code is going to
-PRINT
-Welcome to the Palindrome Checker manager System
-Version : 1.0
-
-*@auther JSO
-*@Version 1.0
-*/
-
 public class PalindromeCheckerApp {
-    /**
-     * Application entry point for UC2.
-     * @param args Command-line arguments
-     */
+
     public static void main(String[] args) {
+
         String input = "madam";
+        char[] characters = input.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
         boolean isPalindrome = true;
 
-        // Loop only till half of the string length
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+        while (start < end) {
+            if (characters[start] != characters[end]) {
                 isPalindrome = false;
                 break;
             }
+            start++;
+            end--;
         }
-        //  result
+
         if (isPalindrome) {
             System.out.println(input + " is a Palindrome.");
         } else {
